4a952a9bfa4db243d80a50454307a800ab47dd73
yangjw <yangwen8701@126.com>
2016-03-15 14:04:30 +0800

    增加了注释

--- app/src/main/java/com/example/yangjw/githubdemo/MainActivity.java
+++ app/src/main/java/com/example/yangjw/githubdemo/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //测试二下
+        //测试PULL功能
     }
 }
